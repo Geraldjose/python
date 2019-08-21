9062c42f1a3084ce6bd9e62df825591711ea137b
Gerald <Gerald.cuadra01@gmail.com>
2019-08-21 09:26:17 -0600

    second commit

---
+++ miprimerprograma.py
@@ -0,0 +1,12 @@
+x, book = 100, "I Robot"
+
+print(x, book)
+
+# conventiones
+book_Name = "I Robot" #snake case
+bookname = "Digital fortress" #camel case
+bookname = "asdasdadasdasdasd" #pascal case
+
+PI = 3.1416
+
+book_Name = "
