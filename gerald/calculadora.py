be20533c299e1fc901dc15cdbbbe838ba3e19df0
Gerald <Gerald.cuadra01@gmail.com>
2019-08-22 13:07:05 -0600

    calculato

---
+++ gerald/calculadora.py
@@ -0,0 +1,16 @@
+def calculadora(n1, n2, operator):
+    if operator == "*":
+        print(n1 * n2)
+    elif operator == "+":
+        print(n1 + n2)
+    elif operator == "-":
+        print(n1 - n2)
+    elif operator == "/":
+        print(n1 / n2)
+
+
+
+calculadora(5, 6, "*")
+calculadora(5, 6, "+")
+calculadora(5, 6, "-")
+calculadora(5, 6, "/")
